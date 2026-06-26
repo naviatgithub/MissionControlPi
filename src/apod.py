24a5d8b6e67ee9f2199b9a0f4eccf13f135d3e4e
Navdeep Dasaraju <naviplayz2020@gmail.com>
2026-06-26 16:14:18 -0500

    Added APOD API integration

---
+++ src/apod.py
@@ -0,0 +1,19 @@
+"""
+Astronomy Picture of the Day (APOD) module
+for fetching and displaying the Astronomy Picture of the Day.
+"""
+
+import requests
+
+apiKey = "DEMO_KEY" # The demo key works but has a slower request rate. you can easily get your own api key from https://api.nasa.gov/
+
+url = f"https://api.nasa.gov/planetary/apod?api_key={apiKey}"
+response = requests.get(url).json()
+# print(response.status_code)  # use if the website isn't working, it shows the success (200) or error code
+
+image_url = response["url"]
+image = requests.get(image_url).content
+
+
+with open("apod.jpg", "wb") as f:
+    f.write(image)
